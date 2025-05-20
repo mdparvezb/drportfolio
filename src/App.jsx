cfaa663fdb720f4435c3ed370673b05b2ed78c24
Parvez <mdparvezbyahatti@gmail.com>
2025-05-20 19:40:29 +0530

    file upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
-import Contact from './pages/contact'
 import AboutIr from './pages/AboutIr'
 import AboutDr from './pages/AboutDr'
 import Treatment from './pages/Treatment'
+import Contact from './pages/contact'
 
 function App() {
 
