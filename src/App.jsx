dea8ee1ef1a28f8085e76ec4de0c387e4d0c4be7
Parvez <mdparvezbyahatti@gmail.com>
2025-06-03 21:05:22 +0530

    project updated 03.06.2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
 import AboutIr from "./pages/AboutIr";
 import AboutDr from "./pages/AboutDr";
